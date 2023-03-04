d01791a77f076ebbc369b888abaa6fc40341d671
Mustakim <Mustakimkhan1111176@gmail.com>
2023-03-04 11:42:56 +0530

    adding chinese chess page

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ import { Web3Modal } from "@web3modal/react";
 import { Web3Button } from "@web3modal/react";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 
-import { polygon, polygonMumbai } from "wagmi/chains";
+import { polygon } from "wagmi/chains";
 import { useState, useEffect } from 'react';
 function App() {
   const [balance, setBalance] = useState(null);
   const [isConnected, setIsConnected] = useState(true);
-  const chains = [polygon, polygonMumbai];
+  const chains = [polygon];
 
   // Wagmi client
   const { provider } = configureChains(chains, [
@@ -38,32 +38,38 @@ function App() {
   const ethereumClient = new EthereumClient(wagmiClient, chains);
 
   async function chess() {
-    if (isConnected) {
+    if (balance == null) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      await window.ethereum.enable();
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+      if (chainId !== '0x89') {
+        //alert('Incorrect network! Switch your metamask network to Rinkeby');
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x89' }],
+        })
+      }
+      console.log(chainId);
       const signer = provider.getSigner();
       const address = await signer.getAddress();
       const balance = await provider.getBalance(address);
       let bln = ethers.utils.formatEther(balance)
       setBalance(bln)
       console.log(bln);
-      if (balance < 0.001) {
+    }
+    if (balance < 0.001) {
 
-        let ans = prompt("You don't have enough balance, if you want to buy MATIC press yes")
-        if (ans.toLocaleLowerCase() == "yes") {
-          window.location.href = 'https://quickswap.exchange/#/swap?inputCurrency=0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee&outputCurrency=0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0';
-        }
+      let ans = prompt("You don't have enough balance, if you want to buy MATIC press yes")
+      if (ans.toLocaleLowerCase() == "yes") {
+        window.location.href = 'https://quickswap.exchange/#/swap?inputCurrency=0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee&outputCurrency=0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0';
+      }
 
-      }
-      else {
-        let val = prompt("Enter the MATIC")
-        if (val >= 0.1) {
-          window.location.href = "https://dungtoken.vercel.app/chess.html"
-        }
-      }
     }
     else {
-      alert("Connect wallet first");
+      let val = prompt("Enter the MATIC")
+      if (val >= 0.1) {
+        window.location.href = "https://dungtoken.vercel.app/chess.html"
+      }
     }
   }
   async function page2() {
