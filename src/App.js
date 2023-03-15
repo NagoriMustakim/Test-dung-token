a8dd19c0ec6595ec47d3d1ceefdbcc810f9de3b1
Mustakim <Mustakimkhan1111176@gmail.com>
2023-03-15 22:44:55 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
-
 import './App.css';
 import { ethers } from 'ethers';
-
 import {
   EthereumClient,
   modalConnectors,
@@ -11,13 +8,17 @@ import {
 import { Web3Modal } from "@web3modal/react";
 import { Web3Button } from "@web3modal/react";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-
+import { useAccount } from "wagmi"
 import { polygon } from "wagmi/chains";
-import { ChinessChess } from './ChinessChess';
+import { useState } from 'react';
 
 function App() {
+  const { isConnected } = useAccount()
+  // const [isConnected, setIsConnected] = useState(false)
   const chains = [polygon];
-
+  const connect = () => {
+    // setIsConnected(true)
+  }
   // Wagmi client
   const { provider } = configureChains(chains, [
     walletConnectProvider({ projectId: "4527f727647cf1d44ed39fab8ee2de68" }),
@@ -37,57 +38,68 @@ function App() {
   const ethereumClient = new EthereumClient(wagmiClient, chains);
 
   async function chess() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const chainId = await window.ethereum.request({ method: 'eth_chainId' });
-    if (chainId !== '0x89') {
-      await window.ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: '0x89' }],
-      })
-    }
-
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
-    const balance = await provider.getBalance(address);
-    let bln = ethers.utils.formatEther(balance)
-
-    if (balance < 0.001) {
-
-      let ans = prompt("You don't have enough balance, if you want to buy MATIC press yes")
-      if (ans.toLocaleLowerCase() == "yes") {
-        window.location.href = 'https://quickswap.exchange/#/swap?inputCurrency=0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee&outputCurrency=0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0';
+    if (isConnected) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+      if (chainId !== '0x89') {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x89' }],
+        })
       }
 
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      const balance = await provider.getBalance(address);
+      let bln = ethers.utils.formatEther(balance)
+
+      if (bln < 0.001) {
+
+        let ans = prompt("You don't have enough balance, if you want to buy MATIC press yes")
+        if (ans.toLocaleLowerCase() == "yes") {
+          window.location.href = 'https://quickswap.exchange/#/swap?inputCurrency=0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee&outputCurrency=0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0';
+        }
+
+      }
+      else {
+        window.location.href = "http://chess.dungtoken.com:8080/"
+      }
     }
     else {
-      window.location.href = "http://chess.dungtoken.com:8080/"
+      alert("Connect your wallet")
     }
   }
+
   async function chinessChess() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await window.ethereum.enable();
-    const chainId = await window.ethereum.request({ method: 'eth_chainId' });
-    if (chainId !== '0x89') {
-      await window.ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: '0x89' }],
-      })
-    }
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
-    const balance = await provider.getBalance(address);
-    let bln = ethers.utils.formatEther(balance)
-
-    if (balance < 0.001) {
-      let ans = prompt("You don't have enough balance, if you want to buy MATIC press yes")
-      if (ans.toLocaleLowerCase() == "yes") {
-        window.location.href = 'https://quickswap.exchange/#/swap?inputCurrency=0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee&outputCurrency=0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0';
+    if (isConnected) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await window.ethereum.enable();
+      const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+      if (chainId !== '0x89') {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x89' }],
+        })
       }
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      const balance = await provider.getBalance(address);
+      let bln = ethers.utils.formatEther(balance)
 
+      if (bln < 0.001) {
+        let ans = prompt("You don't have enough balance, if you want to buy MATIC press yes")
+        if (ans.toLocaleLowerCase() == "yes") {
+          window.location.href = 'https://quickswap.exchange/#/swap?inputCurrency=0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee&outputCurrency=0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0';
+        }
+
+      }
+      else {
+        window.location.href = "https://dungtoken.vercel.app/ChineseChess.html"
+      }
     }
     else {
-      window.location.href = "https://dungtoken.vercel.app/ChineseChess.html"
+      alert("Connect your wallet")
     }
 
   }
@@ -96,7 +108,6 @@ function App() {
 
     <>
       <WagmiConfig client={wagmiClient}>
-        {/* <HomePage /> */}
 
       </WagmiConfig>
 
@@ -108,13 +119,14 @@ function App() {
       <h2>You need to HODL Matic Token in your wallet to play</h2>
       <div className="btn">
 
-        <Web3Button></Web3Button>
+        <Web3Button>
+        </Web3Button>
       </div>
-      <div class="container">
-        <div class="ov">
+      <div className="container">
+        <div className="ov">
           <a className="a1" href='#' onClick={chess}>Play Chess</a>
           <a className="a2" href='#' onClick={chinessChess}>Play Chinese Chess</a>
-
+          {console.log(isConnected)}
         </div>
       </div>
     </>
--- src/ChinessChess.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ChinessChess = () => {
-    return (
-        <div>ChinessChess</div>
-    )
-}
