97e505ae54bdd5de003caf5eacc9410cc6b6c424
Mustakim <Mustakimkhan1111176@gmail.com>
2023-03-26 09:34:34 +0530

    resolve  exact path error

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import {
   Route,
   BrowserRouter
 } from 'react-router-dom';
-import {Homepage1} from './home/test/Homepage1';
+import { Homepage1 } from './home/test/Homepage1';
+import { Test } from './Component/Test';
 function App() {
   return (
-      <BrowserRouter>
-        <div >
-          <Routes>
-            <Route path="home/test/homepage1.html" element={< Homepage1 />}></Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div >
+        <Routes>
+          <Route path="/" element={< Test />}></Route>
+          <Route path="home/test/homepage1.html" element={< Homepage1 />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 
 
