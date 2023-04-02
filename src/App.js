654d41ce94661d27e724b1640f202c3065395e95
Mustakim <Mustakimkhan1111176@gmail.com>
2023-04-02 07:57:14 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Test from './Component/Test'
 import Homepage1 from './Home/Test/Homepage1'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Test />}></Route>
-        <Route path='/home/test/homepage1.html' element={<Homepage1 />}></Route>
+        <Route path='/' element={<Homepage1 />}></Route>
       </Routes>
     </BrowserRouter>
   )
